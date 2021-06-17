130b4e60c3b5a43cde84056f5c787ea453cb7799
venkatamahesh <mahesh@gmail.com>
2021-06-17 15:38:19 +0530

    msg

--- SQLCommands/SQLQuery1.sql
+++ SQLCommands/SQLQuery1.sql
@@ -1,6 +1,6 @@
 create database employee
 use employee
-create table employee_tb1
+create table employee_table1
 ( 
   Eid int primary key,
   Fname varchar(50),
@@ -8,14 +8,18 @@ create table employee_tb1
   Salary float,
   Phonenumber varchar(15)
 )
-insert into employee_tb1 values(1,'mahesh','dasari',500000,'9701306667')
-select * from employee_tb1
-insert into employee_tb1 values(2,'chandu','dasari',50000,'9701306664')
-insert into employee_tb1 values(3,'sai','dasari',5000,'9701306663')
-delete from employee_tb1 where Eid=3
-update employee_tb1 set Fname='sai' where Eid=2
-SP_HELP employee_tb1
-Alter table employee_tb1 alter column fname varchar(20)
-Alter table employee_tb1 add eaddress varchar(50)
-SP_RENAME'employee_tb1.Fname','firstname'
-SP_RENAME'employee-table','employee_tb1'
+insert into employee_table1 values(1,'mahesh','dasari',500000,'9701306667')
+select * from employee_table1
+insert into employee_table1 values(2,'chandu','dasari',50000,'9701306664')
+insert into employee_table1 values(3,'sai','dasari',5000,'9701306663')
+alter table employee_table1 alter column phonenumber varchar(20)
+alter table employee_table1 add Eage int
+SP_RENAME'employee_table1.Fname','firstname'
+alter table employee_table1 drop column Eage
+select * from employee_table2
+Truncate table employee_table2
+drop table employee_table2
+update employee_table1 set firstname='venkat' where Eid=1
+update employee_table1 set Salary=400000
+delete from employee_table1 where Eid=3
+delete from employee_table1
